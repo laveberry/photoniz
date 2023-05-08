4d2a89fb826f825d759ad2efa9e7e7c756a0b22c
laveberry <laveberry7@gmail.com>
2023-05-08 21:08:08 +0900

    초기 프로젝트 생성

---
+++ src/main/java/com/laveberry/photoniz/PhotonizApplication.java
@@ -0,0 +1,13 @@
+package com.laveberry.photoniz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotonizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotonizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laveberry/photoniz/PhotonizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laveberry.photoniz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotonizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
