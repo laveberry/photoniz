69e3dd9327741b1c625d4a3699f9f7d31cbbd286
laveberry <laveberry7@gmail.com>
2023-05-10 22:08:46 +0900

    리액트 연동 테스트

---
+++ src/main/java/com/laveberry/photoniz/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.laveberry.photoniz.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//프론트 연동 테스트 위한 컨트롤러
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/api/hello")
+    public String test(){
+        return "Hello, world!";
+    }
+
+}
