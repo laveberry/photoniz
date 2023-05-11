28920e12226f53faf8d53e050f39f5802e33f7de
laveberry <laveberry7@gmail.com>
2023-05-11 22:14:25 +0900

    헤더/푸터 초기

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,6 +2,9 @@
 
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import SearchHeader from "./components/Common/SearchHeader";
+import Layout from "./components/Common/Layout";
+
 
 function App() {
   const [hello, setHello] = useState('')
@@ -13,9 +16,9 @@ function App() {
   }, []);
 
   return (
-      <div>
+      <Layout>
         백엔드에서 가져온 데이터입니다 : {hello}
-      </div>
+      </Layout>
   );
 }
 
---
+++ src/main/frontend/src/components/Common/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+    return (
+        <header>
+            <div>
+                로고 자리
+            </div>
+
+            <nav>
+                <ul>
+                    <li>메뉴1</li>
+                    <li>2</li>
+                    <li>3</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Header
