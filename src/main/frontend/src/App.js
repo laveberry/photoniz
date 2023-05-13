122e623a221efb0654bd9a380f97d730dae60b39
laveberry <laveberry7@gmail.com>
2023-05-13 14:09:36 +0900

    타입 스크립트 세팅

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -4,6 +4,7 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import SearchHeader from "./components/Common/SearchHeader";
 import Layout from "./components/Common/Layout";
+import HomeContainer from "./components/Common/HomeContainer";
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
 
   return (
       <Layout>
+          <HomeContainer></HomeContainer>
         백엔드에서 가져온 데이터입니다 : {hello}
       </Layout>
   );
---
+++ src/main/frontend/src/components/Common/HomeContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomeContainer  = () => {
+    return (
+        <div>
+            <h2>홈컨테이너</h2>
+        </div>
+    )
+}
+
+export default HomeContainer
