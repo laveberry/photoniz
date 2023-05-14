4d376a6857fae7ab456b0900a3b2c59e2452c026
kadun1 <kadunjj@gmail.com>
2023-05-14 11:35:17 +0900

    test: 테스트 코드 추가

---
+++ src/main/java/com/laveberry/photoniz/service/UserService.java
@@ -0,0 +1,7 @@
+package com.laveberry.photoniz.service;
+
+
+public interface UserService {
+
+    String join(String email, String name, String password);
+}
---
+++ src/test/java/com/laveberry/photoniz/sample/service/ServiceTest.java
@@ -0,0 +1,21 @@
+package com.laveberry.photoniz.sample.service;
+
+import com.laveberry.photoniz.sample.BaseSpringBootTest;
+import com.laveberry.photoniz.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class ServiceTest extends BaseSpringBootTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void 회원가입서비스() {
+        String user = userService.join("user@naver.com", "사람", "1234");
+        assertThat(user).isEqualTo("사람");
+    }
+}
