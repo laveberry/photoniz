1c30884b2137bf0465d0f6e88214bdb0c397b273
kadun1 <kadunjj@gmail.com>
2023-06-09 00:03:07 +0900

    feat: 파일업로드 기본 구현

--- src/main/java/com/laveberry/photoniz/common/util/FileUploader.java
+++ src/main/java/com/laveberry/photoniz/common/util/FileUploader.java
@@ -1,21 +1,35 @@
 package com.laveberry.photoniz.common.util;
 
+import com.laveberry.photoniz.exception.CustomException;
+import com.laveberry.photoniz.exception.ExceptionType;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Component
+@Slf4j
 public class FileUploader {
 
     @Value("${upload.path}")
-    private String path;
+    private String basicPath;
 
     private final String DELIMITER = File.separator;
 
-    public void upload(String fileName, MultipartFile file) {
-        final String fileFullPath = path + DELIMITER + fileName;
-        System.out.println("fileFullPath = " + fileFullPath);
+    public void upload(String fileName, MultipartFile multipartFile) {
+        final String fileFullPath = basicPath + DELIMITER + fileName;
+
+        try {
+            Files.createDirectories(Paths.get(basicPath)); //FIXME 파일경로 수정
+            multipartFile.transferTo(new File(fileName));
+        } catch (IOException e) {
+            log.error("파일 업로드 실패 : ", e);
+            throw new CustomException(ExceptionType.SIGN_IN_FAILED);
+        }
     }
 }
--- src/main/java/com/laveberry/photoniz/exception/ExceptionType.java
+++ src/main/java/com/laveberry/photoniz/exception/ExceptionType.java
@@ -15,7 +15,8 @@ public enum ExceptionType {
     USER_DELETE_FAIL(HttpStatus.BAD_REQUEST, "U1003", "사용자 삭제에 실패하였습니다."),
     ADMIN_DO_NOT_DELETE(HttpStatus.BAD_REQUEST, "U1004", "관리자는 삭제할 수 없습니다."),
     BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "B1000", "게시물이 존재하지 않습니다."),
-    INCORRECT_BOARD_TYPE(HttpStatus.BAD_REQUEST, "B1001", "게시판 타입이 적절하지 않습니다.");
+    INCORRECT_BOARD_TYPE(HttpStatus.BAD_REQUEST, "B1001", "게시판 타입이 적절하지 않습니다."),
+    FILE_UPLOAD_FAIL(HttpStatus.BAD_REQUEST, "F1000", "파일 업로드에 실패했습니다.");
 
     private final HttpStatus status;
     private final String errorCode;
