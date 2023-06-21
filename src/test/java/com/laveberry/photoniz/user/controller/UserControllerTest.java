4630529c489638ce98d48e7d0f6f74fd18b6ad1d
kadun1 <kadunjj@gmail.com>
2023-06-22 00:07:25 +0900

    test: 유저 컨트롤러 작업

---
+++ src/test/java/com/laveberry/photoniz/user/controller/UserControllerTest.java
@@ -0,0 +1,19 @@
+package com.laveberry.photoniz.user.controller;
+
+import com.laveberry.photoniz.common.BaseSpringBootTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("유저 컨트롤러")
+public class UserControllerTest extends BaseSpringBootTest {
+
+    @Test
+    @DisplayName("유저 가입")
+    void signUp() {
+        //given
+
+        //when
+
+        //then
+    }
+}
