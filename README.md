46fea78cb8e474e45596e8029fb4ff9d706341ed
kadun1 <kadunjj@gmail.com>
2023-06-27 21:51:00 +0900

    feat: board 접근 필터 제외

--- README.md
+++ README.md
@@ -4,11 +4,13 @@
 ## 기술스택
 - [Publishing] https://demos.creative-tim.com/now-ui-dashboard-react/#/dashboard
 - [FrontEnd] React.js
-- [BackEnd] java, SpringBoot, JPA, JWT
+- [BackEnd] Java 17, SpringBoot, Spring Data JPA, QueryDSL, Gradle
+- [Test] Junit, DBUnit
+- [인증/인가] Spring Security, JWT
 - [DataBase] MariaDB
 - [DB 형상관리] flyway
 - [협업툴] Git(public)
 - [Server] AWS EC2
-- [(미정)] ci/cd : 젠킨스
-- [erd] erdcloud
+- [(미정)] ci/cd : 젠킨스 or Github Action
+- [erd] erdcloud(https://erdcloud.com/)
 - [(미정)] HTTPS 인증구축
--- src/main/java/com/laveberry/photoniz/config/SecurityConfig.java
+++ src/main/java/com/laveberry/photoniz/config/SecurityConfig.java
@@ -16,6 +16,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -38,11 +43,12 @@ public class SecurityConfig {
                 .httpBasic().disable()
                 .authorizeHttpRequests(request ->
                         request.requestMatchers("/v1/user/signUp", "/v1/user/signIn",
+                                        "/v1/board/list", "/v1/board/detail/**",
                                         "/v1/photoBoard/list", "/v1/photoBoard/detail/**").permitAll() // 이 요청은 허용 (회원가입, 로그인)
                                 .anyRequest().authenticated()) // 모든 권한은 인증되어야 함
                 .logout(Customizer.withDefaults()) // 로그아웃시
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
-                        UsernamePasswordAuthenticationFilter.class); // jwt 토큰 인증 필터를 먼저 실행
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), // jwt 토큰 인증 필터를 먼저 실행
+                        UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
